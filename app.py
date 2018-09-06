0667ad9478f93be4c559fd28e2825a49c4d11237
syao <sy2743@columbia.edu>
2018-09-06 07:29:51 -0400

    added dynamic endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +7,26 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
+
+
+@app.route('/pokemon/<int:id>', methods=['GET'])
+def get_name(id):
+    r = requests.get("http://pokeapi.co/api/v2/pokemon/%d" % id)
+    name = r.json().get("name")
+    res = "Cannot find pokemon with id %d" % id
+    if name:
+        res = "The pokemon with id %d is %s" % (id, name)
+    return res
+
+
+@app.route('/pokemon/<string:name>', methods=['GET'])
+def get_id(name):
+    r = requests.get("http://pokeapi.co/api/v2/pokemon/%s" % name)
+    id = r.json().get("id",-1)
+    res = "Cannot find pokemon named %s" % name.capitalize()
+    if id != -1:
+        res = "%s has id %d" % (name.capitalize(), id)
+    return res
 
 
 if __name__ == '__main__':
